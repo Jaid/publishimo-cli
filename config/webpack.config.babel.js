7cc652cacbc7b324334e30a9b8a70e30caf7086e
Jaid <jaid.jsx@gmail.com>
2019-01-15 02:43:46 +0100

    Removed unneeded line

--- config/webpack.config.babel.js
+++ config/webpack.config.babel.js
@@ -3,7 +3,6 @@ import path from "path"
 import webpackConfigNode from "webpack-config-node"
 
 const isDevelopment = process.env.NODE_ENV !== "production"
-const fromPackage = directive => path.resolve(__dirname, "..", directive)
 
 export default webpackConfigNode({
   isDevelopment,

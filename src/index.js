abc9f26b731a7ad013de01b246842d4ad2dfb8ac
Jaid <jaid.jsx@gmail.com>
2019-01-17 07:37:10 +0100

    Implemented basic cli functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-const p = require("publishimo")
+import path from "path"
+
+import commander from "commander"
+import publishimo from "publishimo"
+import winston from "winston"
+import jsonColorizer from "json-colorizer"
+
+process.argv |> commander
+  .version(__VERSION__)
+  .option("-c, --cwd [dir]", "Project folder that contains a package.json", ".")
+  .option("-j, --json", "Print generated package")
+  .option("-s, --stats", "Print all stats")
+  .option("-o, --output [dir]", "Output directory for the new package")
+  .option("--color", "Enabled colored output")
+  .parse
+
+// TODO The parameter relations are pretty confusing at the moment.
+const isApi = !commander.color && (commander.json || commander.stats)
+
+const log = winston.createLogger({
+  level: "info",
+  format: isApi ? winston.format.printf(entry => entry.message) : winston.format.cli(),
+  transports: [new winston.transports.Console],
+})
+
+const stats = publishimo({
+  pkg: commander.cwd,
+  output: commander.output,
+})
+
+if (isApi) {
+  if (commander.json) {
+    log.info(JSON.stringify(stats.generatedPkg))
+  }
+  if (commander.stats) {
+    log.info(JSON.stringify(stats))
+  }
+  process.exit(0)
+}
+
+if (stats.sourcePkgLocation) {
+  log.info(`Input package: ${stats.sourcePkgLocation |> path.resolve}`)
+}
+
+if (stats.outputPath) {
+  log.info(`Saved package as ${stats.outputPath |> path.resolve}`)
+}
+
+if (commander.stats) {
+  log.info(JSON.stringify(stats, null, 2) |> jsonColorizer)
+} else if (commander.json) {
+  log.info(JSON.stringify(stats.generatedPkg, null, 2) |> jsonColorizer)
+}
--- test/index.test.js
+++ test/index.test.js
@@ -4,8 +4,7 @@ import coffee from "coffee"
 
 const index = path.resolve(__dirname, "..", "dist")
 
-it("should run", () => coffee.fork(index)
-  .debug()
-  .expect("stdout", "function")
+it("should run", () => coffee.fork(index, ["--json"])
+  .expect("stdout", /"name":"jaid"/i)
   .expect("code", 0)
   .end())

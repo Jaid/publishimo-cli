69291abfc0db5d4914902afec90bbeb2ef44afb8
Jaid <jaid.jsx@gmail.com>
2019-06-16 10:23:56 +0200

    Improved Webpack config
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,9 +1,5 @@
-import webpackConfigJaid from "webpack-config-jaid"
+import configure from "webpack-config-jaid"
 
-const isDevelopment = process.env.NODE_ENV !== "production"
-
-export default webpackConfigJaid({
-  isDevelopment,
-  packageRoot: __dirname,
-  type: "cli",
+export default configure({
+  publishimo: {fetchGithub: true},
 })
